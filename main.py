5603356bc391b8d195cf4ba076dfe4757c3a134b
Dstrasel <siruku@gmail.com>
2017-05-03 13:03:02 -0400

    Add Argparse module and support for getting username from cli.

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ Title: Git Commit Monitor
 Author: Darius Strasel strasel.darius@gmail.com
 """
 
+import models
+import argparse
+import pprint
 
 
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(get_repo_commits(username, 'helloworld-express'))
+def main():
+    parser = argparse.ArgumentParser(prog='Git Commit Monitor', description='Get the commit history of a user for the past day.')
+    parser.add_argument('username', metavar='github-username', type=str,
+                        help='a github username')
+    args = vars(parser.parse_args())
+
+    user = models.User(args['username'])
+
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(user.get_user_commit_history())
+
+if __name__ == "__main__":
+    main()
