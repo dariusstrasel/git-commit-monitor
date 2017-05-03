bc9a52925802c1d21b977eb974bf095e76b94941
Dstrasel <siruku@gmail.com>
2017-05-03 12:04:11 -0400

    Move API and repo functions to models.py for overall program OOP design transition.

--- main.py
+++ main.py
@@ -1,67 +1,8 @@
-import requests
-import json
-import datetime
-import pprint
-
 """
->>> r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
->>> r.status_code
-200
->>> r.headers['content-type']
-'application/json; charset=utf8'
->>> r.encoding
-'utf-8'
->>> r.text
-u'{"type":"User"...'
->>> r.json()
-{u'private_gists': 419, u'total_private_repos': 77, ...}
+Title: Git Commit Monitor
+Author: Darius Strasel strasel.darius@gmail.com
 """
 
-
-def api_get(url, payload):
-    result = requests.get(url, params=payload)
-    print("GET ", result.url, payload)
-    if result.status_code == 200:
-        if result.text == '[]':
-            return None
-        else:
-            return result.json()
-    else:
-        print("API Call returned not '200' status code.")
-        return None
-
-
-def get_user_repos(repo_owner):
-    repo_url = 'https://api.github.com/users/' + repo_owner + '/repos'
-    result = api_get(repo_url, None)
-    return [split_repo_name(repo_name['full_name']) for repo_name in result]
-
-
-def split_repo_name(repo_name):
-    repo_tokens = repo_name.split('/')
-    return repo_tokens[1]
-
-def get_repo_commits(repository_owner, repository_name):
-    # 2017-05-03T11:30:24.321455
-    # datetime.datetime.now().isoformat()
-    params = {
-        'since': '2017-05-01T11:30:24.321455',
-    }
-    repo_commit_url = 'https://api.github.com/repos/' + repository_owner + '/' + repository_name + '/commits'
-    result = api_get(repo_commit_url, params)
-    return result
-
-def return_active_repositories(repository_owner):
-    repositories = get_user_repos(repository_owner)
-    result = []
-    for repository in repositories:
-        result.append(get_repo_commits(repository_owner, repository))
-    return result
-
-
-def process_repo_commits(repo_commit_results):
-    len([commit for commit in repo_commit_results])
-# print(return_active_repositories(username))
 
 
 pp = pprint.PrettyPrinter(indent=4)
