2db72395ef221eff274de8b03dd41fda76690b2e
Dstrasel <siruku@gmail.com>
2017-05-04 14:29:04 -0400

    Add None checking for data mutations;Add error handling for 403;Refactor repo API getter logistics.

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 import pprint
+import secrets
 
 """
 >>> r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
@@ -16,19 +17,23 @@ u'{"type":"User"...'
 {u'private_gists': 419, u'total_private_repos': 77, ...}
 """
 
+
 class User:
     """Class object used to represent a user and their repositories."""
 
     def __init__(self, username):
         self.username = username
-        self.user_repos = self.get_user_repositories()
+        self.user_repos = []
 
     def get_user_repositories(self) -> list:
         """Finds a user's repositories from their github account."""
         repo_owner = self.username
         repo_url = 'https://api.github.com/users/' + repo_owner + '/repos'
         result = api_get(repo_url, None)
-        return [self.split_repository_name(repo_name['full_name']) for repo_name in result]
+        if result:
+            self.user_repos = [self.split_repository_name(repo_name['full_name']) for repo_name in result]
+        else:
+            return None
 
     @staticmethod
     def split_repository_name(repo_name):
@@ -40,10 +45,11 @@ class User:
     def get_repository_commits(self, repository_name):
         """Gets the commit history of a repository via the github API."""
         # 2017-05-03T11:30:24.321455
+        # test time: 2017-05-01T11:30:24.321455
         # datetime.datetime.now().isoformat()
         repository_owner = self.username
         params = {
-            'since': '2017-05-01T11:30:24.321455',
+            'since': datetime.datetime.now().isoformat(),
         }
         repo_commit_url = 'https://api.github.com/repos/' + repository_owner + '/' + repository_name + '/commits'
         result = api_get(repo_commit_url, params)
@@ -51,11 +57,14 @@ class User:
 
     def get_user_commit_history(self):
         """Searches a user's github repos and collects/returns their commit history."""
-        repositories = self.get_user_repositories()
+        self.get_user_repositories()
         result = []
-        for repository in repositories:
-            result.append(self.get_repository_commits(repository))
-        return result
+        if self.user_repos:
+            for repository in self.user_repos:
+                result.append(self.get_repository_commits(repository))
+            return result
+        else:
+            return None
 
     @staticmethod
     def count_repository_commits(repo_commit_results):
@@ -72,6 +81,9 @@ def api_get(url, payload):
             return None
         else:
             return result.json()
+    if result.status_code == 403:
+        print("Status return '403', the Github API limit is likely being throttled.")
+        return None
     else:
-        print("API Call returned not '200' status code.")
+        print("API Call returned '%s', not '200' status code." % (result.status_code))
         return None
