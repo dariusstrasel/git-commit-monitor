4d63494c39a9ff4df8769f7ac90db2d8ddfd405c
Darius Strasel <strasel.darius@gmail.com>
2017-05-13 22:00:05 -0400

    Refactor argparse instance into seperate function; refactor main to use new auth class handler.

--- main.py
+++ main.py
@@ -8,19 +8,34 @@ import argparse
 import pprint
 
 
-def main():
-    parser = argparse.ArgumentParser(prog='Git Commit Monitor', description='Get the commit history of a user for the past day.')
+def parse_input_commands():
+    parser = argparse.ArgumentParser(prog='Git Commit Monitor',
+                                     description='Get the commit history of a user for the past day.')
     parser.add_argument('username', metavar='github-username', type=str,
                         help='a github username')
+    # Turn input arguments into dictionary.
     args = vars(parser.parse_args())
-    user_auth = models.Instance.get_config()
-    user = models.User(args['username'], user_auth)
+    return args
+
+
+def main():
+    """Process input arguments, get user config settings,
+     create User/Instance class instances, and get the commit history for the user.
+     """
+    # Get input arguments, create api instance (checks for secrets info)
+    input_arguments = parse_input_commands()
+    auth_instance = models.Instance()
+
+    queried_user = input_arguments['username']
+
+    # Create User instance and pass in CLI user, followed by an auth instance
+    user = models.User(queried_user, auth_instance)
     user.get_user_repositories()
 
     pp = pprint.PrettyPrinter(indent=4)
 
     if user.user_repos:
-        return pp.pprint(len(user.get_user_commit_history()))
+        print(user.get_user_commit_history())
     else:
         print("No results returned from API.")
 
